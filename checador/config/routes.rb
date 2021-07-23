febb4729b5eecef76ae87ce60d5c15c89f5e3e3e
ManuelAcostaGuerrero <mamanuelma@gmail.com>
2021-07-22 23:44:38 -0500

    Creation of Registration company Interface and Create views for Companies
    I report the total GRUD of the module of the branches

--- checador/config/routes.rb
+++ checador/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :companies
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'admin/dashboard'
